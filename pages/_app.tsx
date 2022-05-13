87aa96a1bf115f64343034b63663809d3daaa3fb
Adem ilter <ademilter@gmail.com>
2022-05-13 12:54:42 +0300

    remove footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Header from "components/Header";
-import Footer from "components/Footer";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   return (
-    <div className="flex min-h-screen flex-col text-zinc-800">
-      <div>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </div>
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
   );
 }
 
